9e1ff545a25d6d1dad88f2a71567cd8eb802cc93
berkagmpp <berkagmpp@gmail.com>
2022-05-08 17:59:32 +1200

    fix useCounter custom hook add the  boolean flag with default value: true as argument / add dependancy in useEffect()

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,18 +1,22 @@
 import { useState, useEffect } from "react";
 
 // custom-hook must start with 'use' in the functioning name
-const useCounter = () => {
-    const [counter, setCounter] = useState(0);
+const useCounter = (forwards = true) => {
+    const [counter, setCounter] = useState(0);          // this useState() will be tighed with useCounter 
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setCounter((prevCounter) => prevCounter + 1);
+            if (forwards) {
+                setCounter((prevCounter) => prevCounter + 1);
+            } else {
+                setCounter((prevCounter) => prevCounter - 1);
+            }
         }, 1000);
 
         return () => clearInterval(interval);
-    }, []);
+    }, [forwards]);
 
-    return <Card>{counter}</Card>;
+    return counter;
 };
 
 export default useCounter;
